48c298719d58889cc3a0b31524ec8fcb16b23ba5
Cian Booth <this.is.cian@gmail.com>
2018-03-10 10:47:29 +0000

    added focus on click

--- map.js
+++ map.js
@@ -1,3 +1,4 @@
+var selected = undefined;
 function makeGrid( xSegments, ySegments, blockSize ) {
     var gridObject = new THREE.Object3D();
     function makeGridBlock( blockSize ) {
@@ -6,7 +7,8 @@ function makeGrid( xSegments, ySegments, blockSize ) {
         var shaderMaterial = new THREE.ShaderMaterial( {
             uniforms: {
                 mouseOver: { value: 0.0 },
-                lineWidth: { value: 0.1 },
+                lineWidth: { value: 0.05 },
+                isSelected: { value: 0 },
             },
             vertexShader: ` 
                 uniform float mouseOver;
@@ -15,7 +17,7 @@ function makeGrid( xSegments, ySegments, blockSize ) {
                     vUv = uv;
                     vec3 pos = position;
                     if(mouseOver > 0.5) {
-                        pos *= 1.2;
+                        pos += vec3(0,3,0);
                     }
                     gl_Position = projectionMatrix * modelViewMatrix * vec4( pos, 1.0 ); 
                 } 
@@ -23,11 +25,16 @@ function makeGrid( xSegments, ySegments, blockSize ) {
             fragmentShader: `
                 varying vec2 vUv;
                 uniform float lineWidth;
+                uniform float mouseOver;
+                uniform float isSelected;
                 void main() {
                     float limit = 0.5 - lineWidth;
                     float xEdge = abs(vUv.x - 0.5 );
                     float yEdge = abs(vUv.y - 0.5 );
-                    vec3 color = vec3(0.5,0,0);
+                    vec3 color = vec3(0.5, mouseOver, 0);
+                    if( isSelected > 0.5 ) {
+                        color = vec3( 1, 0, 0 );
+                    }
 
                     if( xEdge > limit ) {
                         color *= (0.5- xEdge) / lineWidth;
@@ -40,22 +47,7 @@ function makeGrid( xSegments, ySegments, blockSize ) {
             `,
         } );
 
-        var material = new THREE.MeshBasicMaterial();
-		material =		new THREE.MeshPhongMaterial( {
-					color: 0x156289,
-					emissive: 0x072534,
-					side: THREE.DoubleSide,
-					flatShading: true
-				} )
-
-        var lineMaterial = new THREE.LineBasicMaterial( { 
-            color: 0xffffff,
-            transparent: true,
-            opacity: 1.0
-        } );
-        var mesh = new THREE.Object3D();
-        //mesh.add ( new THREE.LineSegments( new THREE.EdgesGeometry( boxGeometry ), lineMaterial ) );
-        mesh.add ( new THREE.Mesh( boxGeometry, shaderMaterial ) );
+        var mesh =  new THREE.Mesh( boxGeometry, shaderMaterial );
 
         return mesh;
     }
@@ -70,34 +62,57 @@ function makeGrid( xSegments, ySegments, blockSize ) {
         }
     }
 
+    function intersectObjects( pointer, objects ) {
+
+        var rect = renderer.domElement.getBoundingClientRect();
+        var x = ( pointer.clientX - rect.left ) / rect.width;
+        var y = ( pointer.clientY - rect.top ) / rect.height;
+
+        var pointerVector = new THREE.Vector2();
+        var ray = new THREE.Raycaster();
+        pointerVector.set( ( x * 2 ) - 1, - ( y * 2 ) + 1 );
+        ray.setFromCamera( pointerVector, camera );
+
+        var intersections = ray.intersectObjects( objects, true );
+        return intersections[ 0 ] ? intersections[ 0 ] : false;
+    }
+
     function onMouseMove( event ) {
-		function intersectObjects( pointer, objects ) {
-
-			var rect = renderer.domElement.getBoundingClientRect();
-			var x = ( pointer.clientX - rect.left ) / rect.width;
-			var y = ( pointer.clientY - rect.top ) / rect.height;
-
-            var pointerVector = new THREE.Vector2();
-            var ray = new THREE.Raycaster();
-			pointerVector.set( ( x * 2 ) - 1, - ( y * 2 ) + 1 );
-			ray.setFromCamera( pointerVector, camera );
-
-			var intersections = ray.intersectObjects( objects, true );
-			return intersections[ 0 ] ? intersections[ 0 ] : false;
-		}
-
         for(var i=0; i<gridObject.children.length; i++) {
-            gridObject.children[i].children[0].material.uniforms.mouseOver.value = 0.0;
+            gridObject.children[i].material.uniforms.mouseOver.value = 0.0;
         }
 
         var intersect = intersectObjects( event, gridObject.children );
-        if( intersect ) {
-            //intersect.object.parent.children[0].material.color.set( '#ff0000' );
+        if( intersect && typeof selected === "undefined" ) {
             intersect.object.material.uniforms.mouseOver.value = 1.0;
         }
     }
 
-    //var mesh = new THREE.Mesh( boxGeometry, material);
+    function onMouseClick( event ) {
+        var intersect = intersectObjects( event, gridObject.children );
+        if( intersect && selected != intersect.object ) {
+            selected = intersect.object;
+            camera.zoom = 10;
+            var p = intersect.object.position;
+            camera.position.set( 100 + p.x, 50 + p.y, 100 + p.z );
+            camera.lookAt( intersect.object.position );
+        } else { 
+            selected = undefined;
+            camera.zoom = 1;
+            camera.position.set( 100, 100, 100 );
+            camera.lookAt( 0, 0, 0 );
+        }
+
+        for(var i=0; i<gridObject.children.length; i++) {
+            gridObject.children[i].material.uniforms.mouseOver.value = 0.0;
+            gridObject.children[i].material.uniforms.isSelected.value = gridObject.children[i] == selected ? 1.0 : 0.0;
+            gridObject.children[i].material.uniforms.lineWidth.value = 0.05 / camera.zoom;
+        }
+
+        camera.updateProjectionMatrix();
+    }
+
     window.addEventListener( 'mousemove', onMouseMove, false );
+    window.addEventListener( 'click', onMouseClick, false );
     return gridObject;
 }
